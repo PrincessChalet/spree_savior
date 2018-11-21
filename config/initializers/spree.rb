e3cfb3205a0102d41f466ca6b44881042442ae1b
PrincessChalet <chaletshelton@gmail.com>
2018-11-20 21:03:21 -0600

    edit

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,7 +13,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
-end
+
 attachment_config = {
 
   s3_credentials: {
